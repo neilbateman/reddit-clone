55ecd73cdf44f7ab1226280f3e5dae871b3e0f69
Neil Bateman <neilbatman@gmail.com>
2019-06-14 15:43:06 -0700

    create makePost method to display posts, and sortPost method to sort based on votes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,11 @@ export class AppComponent {
       new Post('Sweet Post', 'awesome.com', 5)
     ];
   }
+  makePost(title: HTMLInputElement, link: HTMLInputElement): boolean{
+    this.posts.push(new Post(title.value, link.value, 0));
+    return false;
+  }
+  sortPosts(): Post[] {
+  return this.posts.sort((a: Post, b: Post) => b.votes - a.votes);
+}
 }
