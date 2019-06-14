7efaf5de830c02aaab9c36af55dbbf9c65a5b4a4
Neil Bateman <neilbatman@gmail.com>
2019-06-14 11:08:22 -0700

    create upVote and downVote methods

--- src/app/post/post.model.ts
+++ src/app/post/post.model.ts
@@ -5,9 +5,16 @@ export class Post {
   link: string;
   votes: number;
 
-constructor(title: string, link: string, votes?: number) {
+  constructor(title: string, link: string, votes?: number) {
   this.title = title;
   this.link = link;
   this.votes = votes || 0;
 }
+  upVote(): void {
+    this.votes +=1;
+  }
+
+  downVote(): void {
+    this.votes +=1;
+  }
 }
