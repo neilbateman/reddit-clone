2657099471fcfc13c80c7cd3a6688877ffb9f2c4
Neil Bateman <neilbatman@gmail.com>
2019-06-14 10:57:29 -0700

    refactored post.ts to post.model.ts to contain constructor and methods for post component

---
+++ src/app/post/post.model.ts
@@ -0,0 +1,13 @@
+import { Post} from '../post';
+
+export class Post {
+  title: string;
+  link: string;
+  votes: number;
+
+constructor(title: string, link: string, votes?: number) {
+  this.title = title;
+  this.link = link;
+  this.votes = votes || 0;
+}
+}
