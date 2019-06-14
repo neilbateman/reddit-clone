420d619a3fccff654fdf400054fb30970af71e76
Neil Bateman <neilbatman@gmail.com>
2019-06-14 11:14:26 -0700

    create Post array and constructor to display dummy posts and submit new posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { Post } from '.post.ts'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  posts: Post[];
   title = 'reddit-clone';
+
+  constructor() {
+    this.posts = [
+      new Post('Sweet Post', 'awesome.com', 5)
+    ];
+  }
 }
