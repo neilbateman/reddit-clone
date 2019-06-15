562c8c756f2c667112de213d3da275549abe5b07
neilbateman <46337141+neilbateman@users.noreply.github.com>
2019-06-14 17:04:58 -0700

    code review commit

---
+++ post.model.ts
@@ -0,0 +1,25 @@
+export class Post {
+  title: string;
+  link: string;
+  votes: number;
+
+  constructor(title: string, link: string, votes?: number) {
+  this.title = title;
+  this.link = link;
+  this.votes = votes || 0;
+}
+
+  webLink(): string {
+    let link: string = this.link.split('//')[1];
+    return link.split('/')[0];
+  }
+  upVote(): void {
+    this.votes +=1;
+  }
+
+  downVote(): void {
+    this.votes +=1;
+  }
+
+
+}
