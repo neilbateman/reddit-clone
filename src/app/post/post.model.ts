1ac7bc918c8e7038c9ed7cb52913dff93dab69fa
Neil Bateman <neilbatman@gmail.com>
2019-06-17 23:23:42 -0700

    changed comment em

--- src/app/post/post.model.ts
+++ src/app/post/post.model.ts
@@ -10,19 +10,10 @@ export class Post {
   this.link = link;
   this.votes = votes;
 }
-  // 
-  // webLink(): string {
-  //   try {
-  //     const link: string = this.link.split('//')[1];
-  //     return link.split('/')[0];
-  //   } catch (err) {
-  //     return null;
-  //   }
-  // }
   upVote(): void {
     this.votes +=1;
   }
-
+  
   downVote(): void {
     this.votes -=1;
   }
